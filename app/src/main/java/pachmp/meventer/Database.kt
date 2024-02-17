683fc810faaa55ede360a21f899ec763238d632e
PytoByte <78215689+PytoByte@users.noreply.github.com>
2024-02-17 20:29:47 +0700

    suspend

--- app/src/main/java/pachmp/meventer/Database.kt
+++ app/src/main/java/pachmp/meventer/Database.kt
@@ -1,19 +1,19 @@
 package pachmp.meventer
 
 class Database {
-    fun registerRequest(email: String, password: String): Boolean {
+    suspend fun registerRequest(email: String, password: String): Boolean {
         return true
     }
 
-    fun confirmRegister(code: Int): Boolean {
+    suspend fun confirmRegister(code: Int): Boolean {
         return true
     }
 
-    fun createUser(nickname: String): Response<String> {
+    suspend fun createUser(nickname: String): Response<String> {
         return Response(ResultResponse(200, "yes"),data="123")
     }
 
-    fun login(email: String, password: String): Response<String> {
+    suspend fun login(email: String, password: String): Response<String> {
         return Response(ResultResponse(200, "yes"),data="123")
     }
 }
